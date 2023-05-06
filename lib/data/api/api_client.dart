38e508d08df838f598e7e91124f92efa87f1f4f9
martin-ngigi <martinwainaina001@gmail.com>
2023-05-06 10:21:28 +0300

    Added depencies/packages

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,60 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../pages/app_constants.dart';
+
+
+class ApiClient extends GetConnect implements GetxService{
+  late String token;
+  late String appBaseUrl;
+  late SharedPreferences sharedPreferences;
+
+  late Map<String, String> _mainHeaders;
+
+  ApiClient({required this.appBaseUrl, required this.sharedPreferences}){
+   baseUrl = appBaseUrl;
+   timeout = Duration(seconds: 30);
+   // token = AppConstants.TOKEN;
+   // token = sharedPreferences.getString(AppConstants.TOKEN)??"";
+
+   _mainHeaders = {
+     "Content-Type": "application/json; charset=UTF-8",
+     "Authorization": "Bearer $token",
+   };
+  }
+
+  void updateHeader(String token ){
+    _mainHeaders = {
+      "Content-Type": "application/json; charset=UTF-8",
+      "Authorization": "Bearer $token",
+    };
+  }
+
+  Future<Response> getData(String uri, {Map<String, String>? headers}) async{ //{Map<String, String>? headers} is an optional argument
+    try{
+      Response response = await get(
+          uri,
+        headers: headers??_mainHeaders // if headers is empty, then use _mainHeaders
+      ); // "get(uri)" is the HTTP GET request
+      print("-------->[ApiClient] Response Body:  ${response.body.toString()}");
+      return response;
+    }
+    catch(e){
+      return Response(statusCode: 1, statusText: e.toString());
+    }
+  }
+
+  //sending data to the server
+  Future<Response> postData(String uri, dynamic body) async {
+    print("1. [ApiClient]"+body.toString());
+    try{
+      Response response = await post(uri, body, headers: _mainHeaders);
+      print("2. [ApiClient] posting. "+response.body.toString());
+      return response;
+    }
+    catch(e){
+      print(e.toString());
+      return Response(statusCode: 1, statusText: "Error getting data:: "+e.toString());
+    }
+  }
+}
---
+++ lib/pages/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants{
+
+  /// ios simulator
+  // static const String BASE_URL="http://127.0.0.1:8000";
+  /// android simulator
+  // static const String BASE_URL="http://10.0.2.2:8000";
+
+  static const String BASE_URL="";
+
+}
